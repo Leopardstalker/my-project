3c244358c88a91aad8883eb183e1a9d61de8f853
Leopardstalker <valentingean1999@mail.ru>
2021-04-02 19:43:45 +0300

    New Router and Layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
+import Navbar from './component/Navbar';
 import './App.css';
+import { BrowserRouter as Router, Switch,
+Route  } from 'react-router-dom';
+import Home from './component/pages/Home';
+import Reports from './component/pages/Reports';
+import Products from './component/pages/Products';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+     <Navbar />
+     <Switch>
+       <Route path='/' exact component={Home}/>
+       <Route path='/reports' component={Reports}/>
+       <Route path='/products' component={Products}/>
+     </Switch>
+     </Router>
+     </>
   );
 }
 
 export default App;
+
+
